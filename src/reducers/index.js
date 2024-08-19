4a4d2e99bde44df4efbf1b49ca9ff5d5c97adb08
KOOK5550 <apilearn0@gmail.com>
2024-08-19 14:27:52 +0600

    Create index.js

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import bird from './bird';
+import game from './game';
+import pipe from './pipe';
+
+export default combineReducers({
+  bird,
+  game,
+  pipe,
+});
