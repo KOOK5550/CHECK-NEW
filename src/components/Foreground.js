0b6b5e2c5e03d3ec8695124520de27e99b0241d9
KOOK5550 <apilearn0@gmail.com>
2024-08-19 14:18:47 +0600

    Create Foreground.js

---
+++ src/components/Foreground.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Foreground = () => (
+  <div style={{ position: 'absolute', bottom: 0, width: '100%' }}>
+    <img src="/path/to/foreground.png" alt="foreground" />
+  </div>
+);
+
+export default Foreground;
